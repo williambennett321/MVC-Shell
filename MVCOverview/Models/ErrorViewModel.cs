e385a149b64fa166d22c236775b16867013a6289
lilbi <lilbi@DESKTOP-U4DJP4P>
2022-01-26 16:19:51 -0500

    Add project files.

---
+++ MVCOverview/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCOverview.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
